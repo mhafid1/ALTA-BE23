2499a4a8bfc31973af5f5e74e10edc57aff6702f
hafid <syl.hafid@gmail.com>
2024-05-25 16:24:33 +0700

    problem 345

--- belajar golang/problem4/main.go
+++ belajar golang/problem4/main.go
@@ -1,14 +1,30 @@
 package main
 
-func isPrime(n int) bool {
-	if n <= 1 {
-		return false
-	}
-	for i := 2; i < n; i++ {
-		if n%i == 0 {
+import (
+	"fmt"
+	"strings"
+)
+
+func palindrome(input string) bool {
+	input = strings.ToLower(input)
+	runes := []rune(input)
+
+	for i := 0; i < len(runes)/2; i++ {
+		if runes[i] != runes[len(runes)-1-i] {
 			return false
 		}
 	}
 	return true
+}
 
+func main() {
+	var text string
+	fmt.Println("masukkan kata")
+	fmt.Scanln(&text)
+
+	if palindrome(text) {
+		fmt.Println(text, "benar kata tersebut palindrome")
+	} else {
+		fmt.Println(text, "salah kata tersebut bukanlah palindrome")
+	}
 }
